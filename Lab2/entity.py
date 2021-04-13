76d705ff417efc1b290a54a00df17dc26cffb920
Zbuer507 <1531427879@qq.com>
2021-04-13 21:21:51 +0800

    add godorsource, add tarot_club

--- Lab2/entity.py
+++ Lab2/entity.py
@@ -21,7 +21,16 @@ class sequence:
         try:
             cursor.execute(sql)
             db.commit()
-            print(sequence_name + "序列已提交名为" + name + "的序列信息")
+            print(sequence_name + "已提交名为" + name + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+    def Insert(db, cursor, sequence_name, road, name):
+        sql = "insert into "+ sequence_name +"(road, name) value(\'" + road + "\', \'" + name + "\');"
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(sequence_name + "已提交名为" + name + "的信息")
         except:
             print("Error in \"" + sql + "\"")
             db.rollback()
@@ -36,7 +45,7 @@ class sequence:
         try:
             cursor.execute(sql)
             db.commit()
-            print(sequence_name + "序列已更新road为" + road + "的信息")
+            print(sequence_name + "已更新road为" + road + "的信息")
         except:
             print("Error in \"" + sql + "\"")
             db.rollback()
@@ -49,7 +58,7 @@ class sequence:
         try:
             cursor.execute(sql)
             db.commit()
-            print(sequence_name + "序列已删除road为" + road + "的信息")
+            print(sequence_name + "已删除road为" + road + "的信息")
         except:
             print("Error in \"" + sql + "\"")
             db.rollback()
@@ -63,7 +72,146 @@ class sequence:
             cursor.execute(sql)
             print(sequence_name + "中途径序号为" + road + "的名称:")
             data = cursor.fetchall()
-            print(data[0]["name"])
+            for item in data:
+                print(item["name"])
+        except:
+            print("Error in \"" + sql + "\"")
+            pass
+
+# 神（天使，圣者）
+# 源质
+class godorsource:
+    def Init(db, cursor, godorsource_name):
+        cursor.execute("drop table if exists " + godorsource_name)
+        sql = """create table """+ godorsource_name +""" (
+                name varchar(30),
+                PRIMARY KEY (name))"""
+        cursor.execute(sql)
+        db.commit()
+        return godorsource_name
+
+    def Insert(db, cursor, godorsource_name):
+        sql = "insert into "+ godorsource_name +"(name) "
+        name = input("name:")
+        sql = sql + "value(\'" + name + "\');"
+        print(sql)
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(godorsource_name + "已提交名为" + name + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+    def Insert(db, cursor, godorsource_name, name):
+        sql = "insert into "+ godorsource_name +"(name) value(\'" + name + "\');"
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(godorsource_name + "已提交名为" + name + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+
+    def Delete(db, cursor, godorsource_name):
+        sql = "DELETE FROM "+ godorsource_name +" WHERE name = "
+        name = input("要删除的name：")
+        sql = sql + "\'" + name + "\';"
+        print(sql)
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(godorsource_name + "已删除name为" + name + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+    
+    def Select(db, cursor, godorsource_name):
+        sql = "select name from "+ godorsource_name
+        print(sql)
+        try:
+            cursor.execute(sql)
+            print(godorsource_name + "中的name:")
+            data = cursor.fetchall()
+            for item in data:
+                print(item["name"])
+        except:
+            print("Error in \"" + sql + "\"")
+            pass
+
+# 塔罗会
+class tarot_club:
+    def Init(db, cursor, tarot_club_name):
+        cursor.execute("drop table if exists " + tarot_club_name)
+        sql = """create table """+ tarot_club_name +""" (
+                codename varchar(2),
+                name varchar(30),
+                PRIMARY KEY (codename))"""
+        cursor.execute(sql)
+        db.commit()
+        return tarot_club_name
+
+    def Insert(db, cursor, tarot_club_name):
+        sql = "insert into "+ tarot_club_name +"(codename, name) "
+        codename = input("codename:")
+        name = input("name:")
+        sql = sql + "value(\'" + codename + "\', \'" + name + "\');"
+        print(sql)
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(tarot_club_name + "已提交名为" + name + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+    def Insert(db, cursor, tarot_club_name, codename, name):
+        sql = "insert into "+ tarot_club_name +"(codename, name) value(\'" + codename + "\', \'" + name + "\');"
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(tarot_club_name + "已提交名为" + name + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+
+    def Update(db, cursor, tarot_club_name):
+        codename = input("需更改信息的codename：")
+        sql = "UPDATE "+ tarot_club_name +" "
+        name = input("请输入新的name：")
+        sql = sql + "set name = \'" + name + "\' "
+        sql = sql + "WHERE codename = \'" + codename + "\';"
+        print(sql)
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(tarot_club_name + "已更新codename为" + codename + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+
+    def Delete(db, cursor, tarot_club_name):
+        sql = "DELETE FROM "+ tarot_club_name +" WHERE codename = "
+        codename = input("要删除的codename：")
+        sql = sql + "\'" + codename + "\';"
+        print(sql)
+        try:
+            cursor.execute(sql)
+            db.commit()
+            print(tarot_club_name + "已删除codename为" + codename + "的信息")
+        except:
+            print("Error in \"" + sql + "\"")
+            db.rollback()
+    
+    def SelectName(db, cursor, tarot_club_name):
+        sql = "select name from "+ tarot_club_name +" where codename = "
+        codename = input("途径序号：")
+        sql = sql + "\'" + codename + "\';"
+        print(sql)
+        try:
+            cursor.execute(sql)
+            print(tarot_club_name + "中途径序号为" + codename + "的名称:")
+            data = cursor.fetchall()
+            for item in data:
+                print(item["name"])
         except:
             print("Error in \"" + sql + "\"")
             pass
